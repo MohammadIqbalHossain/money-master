ea493e47faa063fd07b0976cf62f64f7658eed2b
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-02-17 04:49:24 +0600

    some js added

--- js/script.js
+++ js/script.js
@@ -44,10 +44,10 @@ function updateInput(inputId){
             balance.innerText = incomeValue - expanceSum;
         
 
-        // if(inputId.value < 0 || inputId !== 'number'){
-        //     document.getElementById('validation').classList.add('visible');
+        if(inputId.value < 0 || inputId !== 'number'){
+            document.getElementById('validation').classList.add('visible');
            
-        // }
+        }
            
 
 }
@@ -56,7 +56,9 @@ document.getElementById('save').addEventListener('click', function(){
     const income = document.getElementById('income').value;
     const saveInputValue = document.getElementById('save-input').value;
     const percent = (saveInputValue / income) * 100;
-    // const numericPercent = (percent * 0.01) * income;
+    const neumericPercent = (percent * 0.01) * income;
+    console.log(neumericPercent);
+    
     const saveAmount = document.getElementById('save-amount').innerText = percent;
     const balanceRemain = document.getElementById('balance').innerText;
     
