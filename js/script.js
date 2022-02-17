c29edef5e70ece4a2e8d4fb8a9f984d2d6dd3a70
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-02-17 20:40:05 +0600

    validation added

--- js/script.js
+++ js/script.js
@@ -1,66 +1,87 @@
 document.getElementById('calculate-btn').addEventListener('click', function(){
-    // const foodInput = document.getElementById('food-input');
-    // const rentInput = document.getElementById('rent-input');
-    // const clothInput = document.getElementById('cloth-input');
-    
-    // const foodInputValue = foodInput.value;
-    // const rentInputValue = rentInput.value;
-    // const clothInputValue = clothInput.value
-    
-    // const totalExpenses = document.getElementById('total-expanses');
-    // const expanceSum = totalExpenses.innerText = parseInt(foodInputValue) + parseInt(rentInputValue) + parseInt(clothInputValue);
-    
-    
-    // const income = document.getElementById('income').value;
-    // const incomeValue = parseInt(income);
-    // const balance = document.getElementById('balance');
-    // balance.innerText = incomeValue - expanceSum;
+
+
+
     updateInput('food-input', 'rent-input','cloth-input');
 
   
 
 })
 
+
+
 function updateInput(foodtId, rentId, clothId) {
 
-    if(foodtId, rentId, clothId < 0){
-        // document.getElementById('validation').classList.add('visible');
-        document.getElementById('validation').style.display = 'block';
-    }
-    const foodInput = document.getElementById(foodtId);
-    const rentInput = document.getElementById(rentId);
-    const clothInput = document.getElementById(clothId);
+        const foodInput = document.getElementById(foodtId);
+        const rentInput = document.getElementById(rentId);
+        const clothInput = document.getElementById(clothId);
+        
+        const foodInputValue = parseFloat(foodInput.value);
+        const rentInputValue = parseFloat(rentInput.value);
+        const clothInputValue = parseFloat(clothInput.value);
+
+
+
+        if(foodInputValue, rentInputValue, clothInputValue < 0){
+            document.getElementById('validation').style.display = 'block';
+        }
+        else if(foodInputValue, rentInputValue, clothInputValue == ''){
     
-    const foodInputValue = foodInput.value;
-    const rentInputValue = rentInput.value;
-    const clothInputValue = clothInput.value
-    
-    const totalExpenses = document.getElementById('total-expanses');
-    const expanceSum = totalExpenses.innerText = parseInt(foodInputValue) + parseInt(rentInputValue) + parseInt(clothInputValue);
-    
-    
-    const income = document.getElementById('income').value;
-    const incomeValue = parseInt(income);
-    const balance = document.getElementById('balance');
-    balance.innerText = incomeValue - expanceSum;
+            document.getElementById('str-notify').style.display = 'block';
+        }
+        else{
+            const totalExpenses = document.getElementById('total-expanses');
+            const expanceSum = totalExpenses.innerText = parseInt(foodInputValue) + parseInt(rentInputValue) + parseInt(clothInputValue);
+            
+            const income = document.getElementById('income').value;
+            const incomeValue = parseInt(income);
+            const balance = document.getElementById('balance');
+            balance.innerText = incomeValue - expanceSum;
+            
+
+            if(income < 0 || typeof income !== 'number'){
+                document.getElementById('validation').style.display = 'block';
+            }
+            
+            
+       }
+   
 
 }
 
 
 
 
+
+function  isIncomeGreater(){
+    const totalExpenses = document.getElementById('total-expanses').innerText;
+    const income = document.getElementById('income').value;
+    if(totalExpenses > income){
+        alert('You Can not spend more');
+    }
+}
+
+
+
+
 document.getElementById('save').addEventListener('click', function(){
+    
+   
     const income = document.getElementById('income').value;
     const saveInputValue = document.getElementById('save-input').value;
+
     const neumericPercent = income * parseFloat(saveInputValue / 100)
-    const saveAmount = document.getElementById('save-amount').innerText = neumericPercent;
+
     const balanceRemain = document.getElementById('balance').innerText;
+
+    const saveAmount = document.getElementById('save-amount').innerText = neumericPercent;
+
     document.getElementById('Remain-amount').innerText =  balanceRemain - saveAmount;
 
     if(balanceRemain < saveAmount){
-        alert("isufuceent balance")
+        alert("Insufficient Balance")
     }
     
-
-    
 })
+
+
