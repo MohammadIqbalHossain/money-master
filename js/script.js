355be00c05556e326947918fe0d7481f18fa5b47
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-02-18 20:28:44 +0600

    Changed some UI and funtionality

--- js/script.js
+++ js/script.js
@@ -1,67 +1,73 @@
+//Event listener for calculation.
 document.getElementById('calculate-btn').addEventListener('click', function(){
-
-
-
-    updateInput('food-input', 'rent-input','cloth-input');
-
-  
+   
+    updateInput()
 
 })
 
 
 
-function updateInput(foodtId, rentId, clothId) {
+//Function for calculation. 
+function updateInput(){
+    const foodInput = document.getElementById('food-input');
+    const rentInput = document.getElementById('rent-input');
+    const clothInput = document.getElementById('cloth-input');
 
-        const foodInput = document.getElementById(foodtId);
-        const rentInput = document.getElementById(rentId);
-        const clothInput = document.getElementById(clothId);
+    const totalExpenses = document.getElementById('total-expanses');
+    console.log(totalExpenses.innerText)
         
-        const foodInputValue = foodInput.value;
-        const rentInputValue = rentInput.value;
-        const clothInputValue = clothInput.value;
+    const income = document.getElementById('income').value;
+    const balance = document.getElementById('balance');
+    
+    
 
-        if(foodInputValue, rentInputValue , clothInputValue < 0){
-            document.getElementById('validation').style.display = 'block';
-        }
+    const foodInputValue =  parseInt(foodInput.value);
+    const rentInputValue =  parseInt(rentInput.value);
+    const clothInputValue = parseInt(clothInput.value);
 
-        else if(isNaN(foodInputValue, rentInputValue, clothInputValue == true)){
-            document.getElementById('str-notify').style.display ='block';
-        }
-         
-        else {
-        
+    const inputSum = foodInputValue + rentInputValue + clothInputValue;
+           
+
+    const inputs = [foodInputValue, rentInputValue, clothInputValue];
+    
+
+    if(isNaN(foodInputValue, rentInputValue, clothInputValue)){
+        document.getElementById('str-notify').style.display ='block';
+    }
+    else if(income < inputSum){
+      balance.innerText = 'You can not spend more'
+      balance.style.color = 'white';
+    }
      
-            const totalExpenses = document.getElementById('total-expanses');
-            const expanceSum = totalExpenses.innerText = parseInt(foodInputValue) + parseInt(rentInputValue) + parseInt(clothInputValue);
-            
-            const income = document.getElementById('income').value;
-            const incomeValue = parseInt(income);
-            const balance = document.getElementById('balance');
-            balance.innerText = incomeValue - expanceSum;
+    else {
+        totalExpenses.innerText = inputSum;
+        const incomeValue = parseInt(income);
+        const balance = document.getElementById('balance');
+        balance.innerText = incomeValue - inputSum;
 
-            if(income < 0){
-                document.getElementById('validation').style.display = 'block';
+        for(const input of inputs){
+            if(input < 0){
+            document.getElementById('validation').style.display = 'block';
+            totalExpenses.innerText = 'Please give positive num'
+            
             }
-
+            else if(isNaN(input)){
+                document.getElementById('validation').style.display = 'block';
+                totalExpenses.innerText = '00';
+                balance.innerText = '00';
+            }
             
-          
-        }
-        
-}
-   
+        } 
 
-
-// function  isIncomeGreater(){
-//     const totalExpenses = document.getElementById('total-expanses').innerText;
-//     const income = document.getElementById('income').value;
-//     if(totalExpenses > income){
-//         alert('You Can not spend more');
-//     }
-// }
+    }
+    
+}   
 
 
 
 
+
+//Handleing event for saveings section
 document.getElementById('save').addEventListener('click', function(){
     
    
@@ -73,11 +79,28 @@ document.getElementById('save').addEventListener('click', function(){
     const balanceRemain = document.getElementById('balance').innerText;
 
     const saveAmount = document.getElementById('save-amount').innerText = neumericPercent;
+    
+ 
 
-    document.getElementById('Remain-amount').innerText =  balanceRemain - saveAmount;
+    const ReaminingAmount = document.getElementById('Remain-amount');
+    ReaminingAmount.innerText =  balanceRemain - saveAmount;
 
     if(balanceRemain < saveAmount){
         alert("Insufficient Balance")
+        ReaminingAmount.innerText = '00';
     }
+    else if(saveInputValue < 0 || isNaN(saveInputValue)){
+        alert('Please enter an integr percentage!')
+        ReaminingAmount.innerText = '00';
+    }
+
     
 })
+
+
+
+//Calling function for reset button. 
+function reset(){
+    
+    location.reload();
+}
